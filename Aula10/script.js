985b1651209ab7c17750e5faa8c578b3ea14d279
João Gabriel <jgpns10@gmail.com>
2025-09-02 11:48:30 -0300

    Aula 10

---
+++ Aula10/script.js
@@ -0,0 +1,29 @@
+/**
+ * |Aritméticos
+ * |+ Adição / Concatenação
+ * |** Potenciação
+ * |% Resto da divisão
+ * 
+ * Ordem:
+ * 
+ * |() 
+ * |**
+ * |/ %
+ * |+ -
+ * 
+ * 
+ * Incremento = ++
+ * Decremento = --
+ * 
+ * Operadores de atribuição
+ * 
+ * 
+ * parseInt (Inteiro), parseFloat(decimal)
+ */
+
+let contador = 2;
+contador **= 2;
+contador **= 3;
+
+console.log(contador);
+

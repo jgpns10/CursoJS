f8b06392fad0bdc4226ed0587a7ef73292691025
João Gabriel <jgpns10@gmail.com>
2025-09-05 15:56:10 -0300

    Aula 16 Array

---
+++ Aula16/aula16.js
@@ -0,0 +1,38 @@
+//                0        1        2
+const alunos = ['Luiz', 'Maria', 'João'];
+
+
+
+// console.log(typeof alunos);
+// console.log(alunos instanceof Array); | identificar se é uma Array ou não
+
+// alunos.push('Duda');
+// alunos.push('Mari');
+
+// console.log(alunos.slice(0, -2)); | Fatiar a Array
+
+
+// delete alunos[1]; | Remove o elemento da array sem tirar o indice
+// console.log(alunos);
+
+// const removido = alunos.shift(); | Remove o primeiro elemento da array, junto com o indice
+// const removido = alunos.pop(); | Remove o ultimo elemento de uma array, junto com o indice
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Duda') | Adiciona no começo
+// alunos.unshift('Mari')
+
+// alunos.push('Otávio'); | Adiciona ao fim da Array
+// alunos.push('Duda');
+
+// alunos[alunos.length] = 'Duda'; | Também adiciona ao fim da array(o de cima será usado sempre)
+// alunos[alunos.length] = 'Lucas';
+// alunos[alunos.length] = 'Mari';
+
+// alunos[0] = 'Eduardo'; | altera o elemento do indice da array
+// alunos[3] = 'Duda'; | Adiciona
+
+// console.log(alunos)
+// console.log(alunos[0]);
+// console.log(alunos[2])

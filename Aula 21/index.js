4a32c4107787d4545851acc89913fea135f28abe
João Gabriel <jgpns10@gmail.com>
2025-09-21 12:28:41 -0300

    Inicio do segundo modulo Aula 21

---
+++ Aula 21/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores de comparação
+> - Maior que
+>= - Maior que ou igual
+< - Menor que
+<= - Menor que ou igual
+== - Igualdade (Valor) ***********
+=== - Igualdade estrita (Valor e tipo)
+!= - Diferente (valor) ************
+!== - Diferente estrito (valor e tipo)
+*/
+const num1 = 10;
+const num2 = 10;
+const comp = num1 !== num2;
+console.log(comp)

dfcbf10841bfcedb9695d94ae046154fbde60eeb
João Gabriel <jgpns10@gmail.com>
2025-09-22 09:05:41 -0300

    Aula 22

---
+++ Aula 22/index.js
@@ -0,0 +1,16 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || true || false;
+
+// const usuario = 'Luiz';
+// const senha = '123456';
+
+//  //                True                False
+//const vaiLogar = usuario === 'Luiz' && senha === '12345';
+
+console.log(!false);

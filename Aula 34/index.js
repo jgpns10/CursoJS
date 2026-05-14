47db53809ea16dff11e51cd9f3c5f642275cde93
João Gabriel <jgpns10@gmail.com>
2026-05-14 20:43:13 -0300

    exercicio com For - aula 35

--- Aula 34/index.js
+++ Aula 34/index.js
@@ -1,12 +1,12 @@
-//Estrutura de Repetição
+//Estrutura de Repeticao
 
 // for (let i = 0; i <= 10; i++) {
-//  const par = i % 2 === 0 ? 'par' : 'ímpar';
+//  const par = i % 2 === 0 ? 'par' : 'impar';
 //  console.log(i, par);
 // }
 
-const frutas = ['Maçã', 'Pêra', 'Uva'];
+const frutas = ['Maca', 'Pera', 'Uva'];
 
 for (let i = 0; i < frutas.length; i++) {
-  console.log(`Índice ${i}`, frutas[i])
+  console.log(`indice ${i}`, frutas[i])
 }
---
+++ Aula 35/assets/js/main.js
@@ -0,0 +1,19 @@
+const elementos = [
+    {tag: 'p', texto: 'Nautico'},
+    {tag: 'div', texto: 'Santa'},
+    {tag: 'section', texto: 'Salgueiro'},
+    {tag: 'footer', texto: 'Sport'},
+];
+
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i =0; i<elementos.length; i++) {
+   let {tag, texto} = elementos[i];
+   let tagCriada = document.createElement(tag);
+   tagCriada.innerText = texto;
+   div.appendChild(tagCriada);
+}
+
+container.appendChild(div);

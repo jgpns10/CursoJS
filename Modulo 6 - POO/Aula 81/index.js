4c2ddca9e34762bc97e9663f330b72782b04c415
João Gabriel <jgpns10@gmail.com>
2026-09-17 11:43:48 -0300

    Início do modulo 6 - POO

---
+++ Modulo 6 - POO/Aula 81/index.js
@@ -0,0 +1,23 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+    falar() {
+        console.log(`${this.nome} está falando.`);
+    }
+}
+
+function Pessoa2(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function () {
+    console.log(`${this.nome} está falando.`);
+}
+
+const p1 = new Pessoa('João', 'Gabriel');
+const p2 = new Pessoa2('João', 'Gabriel')
+
+console.log(p1, p2)
